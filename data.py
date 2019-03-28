57c5f2db6ae81ba097254fb3232ea3efa24b2dae
Bradley Wilson <bradleywilson@Bradleys-MBP.home>
2019-03-27 17:36:40 -0700

    added optional param for 'c' and 'u' sequences

--- data.py
+++ data.py
@@ -4,34 +4,50 @@ import numpy as np
 # sequence, ic50, allele
 # todo: specific lengths, alleles, species && ranges for ic50 && non cystine argument
 
-def mhc_dataset():
+def mhc_dataset(remove_c_u = False):
     conn = sql.connect('./mhc.db')
     c = conn.cursor()
-    c.execute('SELECT * FROM mhc_data')
+    if remove_c_u == False:
+        c.execute('SELECT * FROM mhc_data')
+    else:
+        c.execute('SELECT * FROM mhc_data WHERE sequence NOT LIKE \'%C%/\'' +
+                    'AND sequence NOT LIKE \'%U%\'')
     dataset = np.array(c.fetchall())
     conn.close()
     return dataset
 
-def mhc_train():
+def mhc_train(remove_c_u = False):
     conn = sql.connect('./mhc.db')
     c = conn.cursor()
-    c.execute('SELECT sequence, meas, mhc FROM mhc_train')
+    if remove_c_u == False:
+        c.execute('SELECT sequence, meas, mhc FROM mhc_train')
+    else:
+        c.execute('SELECT seqence, meas, mhc FROM mhc_train WHERE sequence' +
+                    'NOT LIKE \'%C%/\' AND sequence NOT LIKE \'%U%\'')
     dataset = np.array(c.fetchall())
     conn.close()
     return dataset.T[0], -np.log10(dataset.T[1].astype(float)), dataset.T[2]
 
-def mhc_test1():
+def mhc_test1(remove_c_u = False):
     conn = sql.connect('./mhc.db')
     c = conn.cursor()
-    c.execute('SELECT sequence, meas, mhc FROM mhc_test1')
+    if remove_c_u == False:
+        c.execute('SELECT sequence, meas, mhc FROM mhc_test1')
+    else:
+        c.execute('SELECT sequence, meas, mhc FROM mhc_test1 WHERE sequence' +
+                    'NOT LIKE \'%C%/\' AND sequence NOT LIKE \'%U%\'')
     dataset = np.array(c.fetchall())
     conn.close()
     return dataset.T[0], -np.log10(dataset.T[1].astype(float)), dataset.T[2]
 
-def mhc_test2():
+def mhc_test2(remove_c_u = False):
     conn = sql.connect('./mhc.db')
     c = conn.cursor()
-    c.execute('SELECT sequence, meas, mhc FROM mhc_test2')
+    if remove_c_u == False:
+        c.execute('SELECT sequence, meas, mhc FROM mhc_test2')
+    else:
+        c.execute('SELECT sequence, meas, mhc FROM mhc_test2 WHERE sequence' +
+                    'NOT LIKE \'%C%/\' AND sequence NOT LIKE \'%U%\'')
     dataset = np.array(c.fetchall())
     conn.close()
     return dataset.T[0], -np.log10(dataset.T[1].astype(float)), dataset.T[2]
