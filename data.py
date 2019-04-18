0e72303069ca0a1f410e97add396eed47e34e2fd
Oliver Nakano-Baker <onakanob@gmail.com>
2019-04-17 23:02:39 -0700

    changed some statements to be more pythonic and added optional filepath inputs

--- data.py
+++ data.py
@@ -4,32 +4,32 @@ import numpy as np
 # sequence, ic50, allele
 # todo: specific lengths, alleles, species && ranges for ic50 && non cystine argument
 
-def mhc_dataset(remove_c = False, remove_u = False, remove_modes = False):
+def mhc_dataset(path='./mhc.db', remove_c=False, remove_u=False, remove_modes=False):
     """
     Parameters: remove every sequence with a 'c'
                 remove every sequence with a 'u'
                 remove the unusual modes of the dataset
     returns the remaining dataset in a numpy array
     """
-    conn = sql.connect('./mhc.db')
+    conn = sql.connect(path)
     c = conn.cursor()
-    if remove_modes == False:
-        if remove_c == False and remove_u == False:
+    if not remove_modes:
+        if not remove_c and not remove_u:
             c.execute('SELECT * FROM mhc_data')
-        elif remove_c == True and remove_u == True:
+        elif remove_c and remove_u:
             c.execute('SELECT * FROM mhc_data WHERE sequence NOT LIKE \'%C%\'' +
                         ' AND sequence NOT LIKE \'%U%\'')
-        elif remove_c == True and remove_u == False:
+        elif remove_c and not remove_u:
             c.execute('SELECT * FROM mhc_data WHERE sequence NOT LIKE \'%C%\'')
         else:
             c.execute('SELECT * FROM mhc_data WHERE sequence NOT LIKE \'%U%\'')
     else:
-        if remove_c == False and remove_u == False:
+        if not remove_c and not remove_u:
             c.execute('SELECT * FROM mhc_data')
-        elif remove_c == True and remove_u == True:
+        elif remove_c and remove_u:
             c.execute('SELECT * FROM mhc_data WHERE inequality != \'>\' AND sequence NOT LIKE \'%C%\'' +
                         ' AND sequence NOT LIKE \'%U%\'')
-        elif remove_c == True and remove_u == False:
+        elif remove_c and not remove_u:
             c.execute('SELECT * FROM mhc_data WHERE inequality != \'>\' AND sequence NOT LIKE \'%C%\'')
         else:
             c.execute('SELECT * FROM mhc_data WHERE inequality != \'>\' AND sequence NOT LIKE \'%U%\'')
@@ -37,32 +37,32 @@ def mhc_dataset(remove_c = False, remove_u = False, remove_modes = False):
     conn.close()
     return dataset
 
-def mhc_train(remove_c = False, remove_u = False, remove_modes = False):
+def mhc_train(path='./mhc.db', remove_c=False, remove_u=False, remove_modes=False):
     """
     Parameters: remove every sequence with a 'c'
                 remove every sequence with a 'u'
                 remove the unusual modes of the dataset
     returns (in order): the amino acid sequences, the -log10 of binding affinities, and the alleles
     """
-    conn = sql.connect('./mhc.db')
+    conn = sql.connect(path)
     c = conn.cursor()
-    if remove_modes == False:
-        if remove_c == False and remove_u == False:
+    if not remove_modes:
+        if not remove_c and not remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_train')
-        elif remove_c == True and remove_u == True:
+        elif remove_c and remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_train WHERE sequence' +
                         ' NOT LIKE \'%C%\' AND sequence NOT LIKE \'%U%\'')
-        elif remove_c == True and remove_u == False:
+        elif remove_c and not remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_train WHERE sequence NOT LIKE \'%C%\'')
         else:
             c.execute('SELECT sequence, meas, mhc FROM mhc_train WHERE sequence NOT LIKE \'%U%\'')
     else:
-        if remove_c == False and remove_u == False:
+        if not remove_c and not remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_train WHERE inequality != \'>\'')
-        elif remove_c == True and remove_u == True:
+        elif remove_c and remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_train WHERE inequality' +
                         ' != \'>\' AND sequence NOT LIKE \'%C%\' AND sequence NOT LIKE \'%U%\'')
-        elif remove_c == True and remove_u == False:
+        elif remove_c and not remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_train WHERE inequality != \'>\'' +
                         ' AND sequence NOT LIKE \'%C%\'')
         else:
@@ -72,32 +72,32 @@ def mhc_train(remove_c = False, remove_u = False, remove_modes = False):
     conn.close()
     return dataset.T[0], -np.log10(dataset.T[1].astype(float)), dataset.T[2]
 
-def mhc_test1(remove_c = False, remove_u = False, remove_modes = False):
+def mhc_test1(path='./mhc.db', remove_c=False, remove_u=False, remove_modes=False):
     """
     Parameters: remove every sequence with a 'c'
                 remove every sequence with a 'u'
                 remove the unusual modes of the dataset
     returns (in order): the amino acid sequences, the -log10 of binding affinities, and the alleles
     """
-    conn = sql.connect('./mhc.db')
+    conn = sql.connect(path)
     c = conn.cursor()
-    if remove_modes == False:
-        if remove_c == False and remove_u == False:
+    if not remove_modes:
+        if not remove_c and not remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_test1')
-        elif remove_c == True and remove_u == True:
+        elif remove_c and remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_test1 WHERE sequence' +
                         ' NOT LIKE \'%C%\' AND sequence NOT LIKE \'%U%\'')
-        elif remove_c == True and remove_u == False:
+        elif remove_c and not remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_test1 WHERE sequence NOT LIKE \'%C%\'')
         else:
             c.execute('SELECT sequence, meas, mhc FROM mhc_test1 WHERE sequence NOT LIKE \'%U%\'')
     else:
-        if remove_c == False and remove_u == False:
+        if not remove_c and not remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_test1')
-        elif remove_c == True and remove_u == True:
+        elif remove_c and remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_test1 WHERE inequality != \'>\' AND sequence ' +
                         'NOT LIKE \'%C%\' AND sequence NOT LIKE \'%U%\'')
-        elif remove_c == True and remove_u == False:
+        elif remove_c and not remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_test1 WHERE inequality != \'>\' AND' +
                         ' sequence NOT LIKE \'%C%\'')
         else:
@@ -107,32 +107,32 @@ def mhc_test1(remove_c = False, remove_u = False, remove_modes = False):
     conn.close()
     return dataset.T[0], -np.log10(dataset.T[1].astype(float)), dataset.T[2]
 
-def mhc_test2(remove_c = False, remove_u = False, remove_modes = False):
+def mhc_test2(path='./mhc.db', remove_c=False, remove_u=False, remove_modes=False):
     """
     Parameters: remove every sequence with a 'c'
                 remove every sequence with a 'u'
                 remove the unusual modes of the dataset
     returns (in order): the amino acid sequences, the -log10 of binding affinities, and the alleles
     """
-    conn = sql.connect('./mhc.db')
+    conn = sql.connect(path)
     c = conn.cursor()
-    if remove_modes == False:
-        if remove_c == False and remove_u == False:
+    if not remove_modes:
+        if not remove_c and not remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_test2')
-        elif remove_c == True and remove_u == True:
+        elif remove_c and remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_test2 WHERE sequence' +
                         ' NOT LIKE \'%C%\' AND sequence NOT LIKE \'%U%\'')
-        elif remove_c == True and remove_u == False:
+        elif remove_c and not remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_test2 WHERE sequence NOT LIKE \'%C%\'')
         else:
             c.execute('SELECT sequence, meas, mhc FROM mhc_test2 WHERE sequence NOT LIKE \'%U%\'')
     else:
-        if remove_c == False and remove_u == False:
+        if not remove_c and not remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_test2')
-        elif remove_c == True and remove_u == True:
+        elif remove_c and remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_test2 WHERE inequality != \'>\' AND sequence ' +
                         'NOT LIKE \'%C%\' AND sequence NOT LIKE \'%U%\'')
-        elif remove_c == True and remove_u == False:
+        elif remove_c and not remove_u:
             c.execute('SELECT sequence, meas, mhc FROM mhc_test2 WHERE inequality != \'>\' AND' +
                         ' sequence NOT LIKE \'%C%\'')
         else:
